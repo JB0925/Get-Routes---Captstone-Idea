b03a71913dc3409ddd71a14901a044bbcb157d30
Jesse Brink <jbrink0925@gmail.com>
2021-09-05 08:24:43 -0400

    add comments to js, make small tweaks to css, add form submit on autocomplete suggestion click.

--- static/js/search.js
+++ static/js/search.js
@@ -4,13 +4,19 @@ const successParagraph = document.querySelector('.success');
 const errorParagraph = document.querySelector('.error');
 const details = document.querySelector('#address-list');
 const formInput = document.querySelector('#street_address');
+const form = document.querySelector('form');
 const bottomSvg = document.querySelector('.bottom');
 
+// set styling if a success or error message appears
+// so as not to push content too far down on page
 if (successParagraph || errorParagraph) {
    h2.style.marginTop = '0%';
    p.style.marginTop = '0%';
 }
 
+// manipulate the value of the bottom SVG's div
+// so that it will be appropriately spaced on the page
+// depending on the results of the autocomplete api calls
 const makeSpaceForAutoComplete = () => {
    if (formInput.value === '') {
       bottomSvg.style.bottom = '0'
@@ -22,12 +28,20 @@ const makeSpaceForAutoComplete = () => {
    }
 }
 
+// gets the details from the autocomplete api and
+// adds them to the DOM
 const getDetails = arr => {
+   // if we get an empty result set, reset the height of
+   // the bottom SVG and return
    if (!arr.length) {
       bottomSvg.style.bottom = '0';
       details.innerHTML = '';
       return;
    }
+
+   // clear the autocomplete details div, 
+   // perform string manipulation on each member
+   // of the array, and add it to the autocomplete div
    details.innerHTML = '';
    let regex = /(NW)|(SE)|(SW)|(NE)/;
    arr.forEach((el, idx) => {
@@ -38,18 +52,27 @@ const getDetails = arr => {
        newDiv.classList.add('choice');
        details.append(newDiv)
        details.style.display = 'block'
-       if (window.innerWidth > 900) {
-          bottomSvg.style.bottom = '-150px'
-       }
    })
+
+   if (window.innerWidth > 900) {
+      bottomSvg.style.bottom = '-150px'
+   }
 }
 
+// if the user selects a choice from the autocomplete
+// dropdown, fill it into the form input, reset appropriate
+// styles, and submit the form.
 const fillInInput = (evt) => {
    formInput.value = evt.target.innerText;
    details.style.display = 'none';
    bottomSvg.style.bottom = '0';
+   form.submit();
 }
 
+
+// set up the DOM style-wise to handle the new autocomplete div,
+// call the api and get results, then add those results to the DOM
+// with "getDetails".
 async function makeAutoComplete() {
    details.style.display = formInput.value === '' ? 'none' : 'block';
    makeSpaceForAutoComplete();
@@ -65,11 +88,13 @@ async function makeAutoComplete() {
    
 }
 
+// Makes it so that the api is not called every time 
+// the user makes a keystroke
 const debounce = (func) => {
    let timer;
    return () => {
        clearTimeout(timer);
-       timer = setTimeout(() => (func()),500);
+       timer = setTimeout(() => (func()),300);
    }
 }
 
